e70e8bf90bdfd125c45c399e9c0f3522196ce1f2
Arun Kumar Gupta <garun540@gmail.com>
2022-12-26 21:18:16 +0530

    initial commit

---
+++ src/main/java/com/kameti/KametiApplication.java
@@ -0,0 +1,13 @@
+package com.kameti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KametiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KametiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kameti/KametiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kameti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KametiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
