cfff8955604e30a419dda7202e072679b30b04bd
Arun Kumar Gupta <garun540@gmail.com>
2022-12-28 22:26:23 +0530

    add local profile add sql db props use jasypt for pwd encryption

--- src/main/java/com/kameti/KametiApplication.java
+++ src/main/java/com/kameti/KametiApplication.java
@@ -1,13 +1,14 @@
 package com.kameti;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KametiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KametiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KametiApplication.class, args);
+    }
 
 }
