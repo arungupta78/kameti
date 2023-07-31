4cc13771561072a3067daee7bb0cdba201548c35
Arun Kumar Gupta <garun540@gmail.com>
2023-07-31 20:47:03 +0530

    used method arguments inside aspect

--- src/main/java/com/demo/advice/SignInAspect.java
+++ src/main/java/com/demo/advice/SignInAspect.java
@@ -1,9 +1,12 @@
 package com.demo.advice;
 
+import com.demo.model.AuthenticationRequest;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -12,6 +15,29 @@ public class SignInAspect {
   @Around("@annotation(com.demo.annotation.LogSignIn)")
   public Object logSignIn(ProceedingJoinPoint joinPoint) throws Throwable {
     System.out.println("User logged in at : " + LocalDateTime.now());
+
+    MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+    /*
+        System.out.println("full method description: " + signature.getMethod());
+        System.out.println("method name: " + signature.getMethod().getName());
+        System.out.println("declaring type: " + signature.getDeclaringType());
+    */
+
+    //    System.out.println("Method args names:");
+    //    Arrays.stream(signature.getParameterNames()).forEach(s -> System.out.println("arg name: "
+    // + s));
+    //
+    //    System.out.println("Method args types:");
+    //    Arrays.stream(signature.getParameterTypes()).forEach(s -> System.out.println("arg type: "
+    // + s));
+
+    System.out.println("Method args values:");
+    Arrays.stream(joinPoint.getArgs())
+        .forEach(o -> System.out.println("arg value: " + o.toString()));
+
+    var req = (AuthenticationRequest) joinPoint.getArgs()[0];
+    System.out.println("Argument received: " + req);
+
     long start = System.currentTimeMillis();
 
     Object proceed = joinPoint.proceed();
